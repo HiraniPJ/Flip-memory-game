315f05818842ec2c7b965826f6b858e029cb90a2
HiraniPJ <pooja.hirani93@gmail.com>
2024-03-19 16:26:52 +0000

    added emojis in java script

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,5 +4,7 @@ const generateGame = () => {
     if (dimensions % 2 !== 0) {
         throw new Error("The dimension of the board must be an even number.");
 }
+// Emoji icons//
+const emojis =['assets/images/camel.png', 'assets/images/cat.png', 'assets/images/corgi.png', 'assets/images/donkey.png','assets/images/elephant.png', 'assets/images/frog.png', 'assets/images/horse.png', 'assets/images/kangaroo.png', 'assets/images/pig.png', 'assets/images/zebra.png'];
 
 };
