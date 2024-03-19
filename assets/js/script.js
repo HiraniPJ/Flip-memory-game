86983db9b3fc7425e89b81ae65afd09fbfdc8846
HiraniPJ <pooja.hirani93@gmail.com>
2024-03-19 16:04:17 +0000

    generated the game board with cards

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+//Generate the game board with cards
+const generateGame = () => {
+    const dimensions = parseInt(selectors.board.getAttribute('data-dimension'), 10);
+    if (dimensions % 2 !== 0) {
+        throw new Error("The dimension of the board must be an even number.");
+}
+
+};
